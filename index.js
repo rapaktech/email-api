1ac517cdd74e1c545b87e9bfa206db5d41437f9f
Jim Ezesinachi <ezesinachijim@gmail.com>
2021-05-19 09:46:05 +0100

    setup node_modules and .env variables

---
+++ index.js
@@ -0,0 +1,12 @@
+const bodyParser = require('body-parser');
+
+const express = require('express');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.listen(PORT, () => console.log("Server is running!")); 
